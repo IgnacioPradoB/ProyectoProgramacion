5d7fd616772b5de00323cb5d9a4c827a3427e88f
IgnacioPradoB <iprado70235@ufide.ac.cr>
2025-07-23 17:55:21 -0600

    Add files via upload

---
+++ Estudiante.java
@@ -0,0 +1,56 @@
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String taller;
+    private boolean activo;
+
+    public Estudiante() {
+        this.id = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.taller = "";
+        this.activo = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTaller() {
+        return taller;
+    }
+
+    public void setTaller(String taller) {
+        this.taller = taller;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+}
